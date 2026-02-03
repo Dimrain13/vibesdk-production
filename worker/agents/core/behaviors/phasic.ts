07abf31af75c4f048f03f265ec26583d03a59bf5
Dimrain13 <daniel.r.millner@gmail.com>
2026-02-02 20:49:11 -0500

    yty

--- worker/agents/core/behaviors/phasic.ts
+++ worker/agents/core/behaviors/phasic.ts
@@ -109,6 +109,7 @@ export class PhasicCodingBehavior extends BaseCodingBehavior<PhasicState> implem
                 name: '',
                 description: '',
                 files: [],
+                lastPhase: true,
             },
         };
         
